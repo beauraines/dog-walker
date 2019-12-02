1b3c6ef97c00a052a328d313a82c393561b484b6
Beau Raines <beau.raines@gmail.com>
2019-12-01 17:42:25 -0800

    Adds services to bookings

--- app/Booking.php
+++ app/Booking.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Booking extends Model
 {
     protected $fillable = [
-        'date', 'user_id',
+        'date', 'user_id', 'service_id'
     ];
 
     public function client()
@@ -15,14 +15,27 @@ class Booking extends Model
         return $this->belongsTo('App\Client', 'user_id', 'id');
     }
 
+    public function service()
+    {
+        return $this->belongsTo(Service::class);
+    }
+
     public function availableStaff()
     {
         $date = $this->date;
         $staffs = Staff::all();
         $availableStaff = $staffs->map(function ($staff) use ($date) {
-            return $staff->available($date) ? $staff : null;
+            return $staff->available($date) ? $staff->fresh() : null;
         });
         return $availableStaff;
+    }
+
+    public function computedPrice()
+    {
+        $petCount = $this->client->pets->count();
+        $service = $this->service;
+        $computedPrice = $service->base_price + ($petCount - 1) * $service->incremental_pet_price;
+        return $computedPrice;
     }
 
     // /**
--- app/Service.php
+++ app/Service.php
@@ -9,4 +9,9 @@ class Service extends Model
     protected $fillable = [
         'name', 'description', 'base_price', 'incremental_pet_price',
     ];
+
+    public function booking()
+    {
+        $this->hasMany('\App\Booking::class');
+    }
 }
