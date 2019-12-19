f8d28171240b59cc90bcea4363eda8cd13ebd13e
Beau Raines <beau.raines@gmail.com>
2019-12-19 11:52:24 -0800

    Adds obiefy/api-response for API responses
    * changes default response property to lower case
    * Changes validation code to 422

---
+++ config/api.php
@@ -0,0 +1,55 @@
+<?php
+
+return [
+
+    /*
+     * Turn to string the status code in the json response's body.
+     */
+    'stringify' => true,
+
+    /*
+     * Set the status code from the json response to be the same as the status code
+     * in the json response's body.
+     */
+    'matchstatus' => false,
+
+    /*
+     * Json response's body labels.
+     */
+    'keys'      => [
+        'status'  => 'status',
+        'message' => 'message',
+        'data'    => 'data',
+    ],
+
+    /*
+     * Default included status codes.
+     */
+    'codes' => [
+        'success'    => 200,
+        'notfound'   => 404,
+        'validation' => 422,
+        'error'      => 500,
+    ],
+
+    /*
+     * Status codes default messages.
+     */
+    'messages' => [
+        'success'    => 'Process is successfully completed',
+        'notfound'   => 'Sorry no results query for your request.',
+        'validation' => 'Validation Failed please check the request attributes and try again.',
+        'error'      => 'Server error, please try again later',
+    ],
+
+    /**
+     * Extra methods
+     */
+    'methods' => [
+        [
+            'code'    => 403,
+            'method'  => 'forbidden',
+            'message' => 'default message'
+        ]
+    ]
+];
