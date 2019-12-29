a4b1228612f931b9758de8d4ac5d3c11b6df64d0
Beau Raines <beau.raines@gmail.com>
2019-12-28 16:11:34 -0800

    Creates PetType controller
    * Adds index method
    * Adds delete method
    * Adds store method
    * adds update method
    * validation and authenticatio in  PetTypeRequest
    	* Adds staff only authorization
    	* FormRequest Validation rule by request method

---
+++ app/Http/Requests/PetTypeRequest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PetTypeRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        switch($this->method()) {
+            case 'GET':
+                return true;
+            break;
+            case 'POST':
+            case 'PUT':
+            case 'PATCH':
+            case 'DELETE':
+                return $this->user()->type == 'App\\Staff' ? true : false;
+            break;
+            default:
+                return false;
+            break;
+        }
+
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $foo = $this->method();
+        switch($this->method()) {
+            case 'GET':
+                return [];
+                break;
+            case 'POST':
+                return [
+                    'pet_type' => 'required|string|unique:pet_types,pet_type',
+                ];
+                break;
+            case 'PUT':
+            case 'PATCH':
+                return [
+                    'pet_type' => 'sometimes|required|string|unique:pet_types,pet_type',
+                ];
+                break;
+                break;
+            case 'DELETE':
+                return [];
+                break;
+            default:
+                return [];
+                break;
+        }
+
+    }
+}
