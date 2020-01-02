e7d4d4750a1b5ece40871dc624ae2c280dd44703
Beau Raines <beau.raines@gmail.com>
2020-01-02 15:58:41 -0800

    Adds Client Controller
    wth Form Request Validation for Client Controller
    
    * PUT, POST api/client validation
    * requres password input for both PUT/POST
    * DELETE - Adds a ClientRequest to the delete so that the Authentication rules can be tested

---
+++ app/Http/Requests/ClientRequest.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ClientRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+
+        switch ($this->method()) {
+            case 'GET':
+            case 'POST':
+            case 'PUT':
+            case 'PATCH':
+            case 'DELETE':
+            default:
+                return $this->user()->type == 'App\\Staff' ? true : false;
+                break;
+        }
+
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $id = $this->route('client');
+
+        switch ($this->method()) {
+            case 'GET':
+                return [];
+                break;
+            case 'POST':
+                return [
+                    'name' => 'required|string',
+                    'email' => 'unique:users',
+                    'type' => 'sometimes|regex:/App\\\\Client/',
+                    'password' => 'required|min:8'
+                ];
+                break;
+            case 'PUT':
+            case 'PATCH':
+                return [
+                    'name' => 'sometimes|required|string',
+                    'email' => 'sometimes|unique:users,email,' . $id,
+                    'type' => 'sometimes|regex:/App\\\\Client/',
+                    'password' => 'sometimes|required|min:8'
+
+                ];
+                break;
+            case 'DELETE':
+                return [];
+                break;
+            default:
+                return [];
+                break;
+        }
+    }
+}
