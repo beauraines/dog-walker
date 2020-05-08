c786541727a2c13186533eec3114a2ebee83c0bc
Beau Raines <beau.raines@gmail.com>
2020-05-08 16:02:03 -0700

    Adds API midddleware to Kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -35,6 +35,10 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+
+            // For Passport - Should remain last
+            \Laravel\Passport\Http\Middleware\CreateFreshApiToken::class,
+
         ],
 
         'api' => [
