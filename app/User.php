0ce5e26346d47fca3cf550a588eee6a9351352ad
beauraines <beauraines@users.noreply.github.com>
2020-05-11 23:49:17 +0000

    Apply php-cs-fixer changes

--- app/User.php
+++ app/User.php
@@ -7,7 +7,6 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
 
-
 class User extends Authenticatable
 {
     use HasApiTokens, Notifiable;
